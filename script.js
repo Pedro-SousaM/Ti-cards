5f415936fec8e74b2f74655cce279fbff8d30289
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2023-11-09 19:48:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -31,6 +31,29 @@ scrollTimeline
         duration: 7,
         ease: "none"
     })
+let pointerTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".boxes",
+        start: "+="+200,
+        end: "+="+800,
+        scrub: true,
+        markers: true,
+    }
+}) 
+    pointerTl.to(".flyPointer",{ 
+        x:0,scale:1,duration:3
+   })
+    .fromTo(".flyPointer",{
+         x:0,duration:2,ease:"power4.out",y:0,rotationZ:120
+    },{
+         x:630, rotationZ:240,duration:13,esase:"power4.in",y:0
+    })
+    .to(".flyPointer",{ 
+         x:560,duration:3,y:400,rotationZ:300,
+    }).to(".flyPointer",{
+        scale:0,duration:4,delay:5
+    })
+    
 
 let textTL = gsap.timeline({
     scrollTrigger: {
@@ -60,11 +83,7 @@ splitChars.forEach((char) => {
             scrub: true,
         },
         opacity: 0,
-        stagger:  {
-            grid: [7,15],
-            from: "edges",
-            amount: 1.5
-          },
+        stagger: 0.1,
         y: 40,
         rotationZ: 13
     })
@@ -72,7 +91,7 @@ splitChars.forEach((char) => {
 const splitChars2 = document.querySelectorAll(".reveal-2")
 splitChars2.forEach((char) => {
     const text = new SplitType(char, { types: 'words,chars' })
-    gsap.to(text.chars, {
+    gsap.from(text.chars, {
         scrollTrigger: {
             trigger: char,
             start: "+="+1900,
@@ -80,11 +99,7 @@ splitChars2.forEach((char) => {
             scrub: true,
         },
         opacity: 0.5,
-        stagger:  {
-            grid: [7,15],
-            from: "random",
-            amount: 1.5
-          },
+        stagger: 0.1,
         color:"#505cfd",  
         textShadow:"6px 6px 0 #000", 
         y:-6, 
@@ -119,7 +134,7 @@ var spn6tl = new TimelineLite({
         start: "+="+200,
         end: "+="+500,
         scrub: false,
-       markers:true
+       markers:false
     }
 });
 spn6tl.from(".spn6",{
@@ -130,7 +145,7 @@ spn6tl.from(".spn6",{
         scrollTrigger: {
             trigger: span,
             start: "+="+1000,
-            end: "+="+600,
+            end: "+="+900,
             scrub: true,
             stagger:0.4,
             delay:1, 
@@ -138,21 +153,30 @@ spn6tl.from(".spn6",{
         }
     });
     spnBtl.from(span,{
-       stagger:0.3, scale:2,opacity: 0,duration:16, ease:"elastic.out(1,0.3)"
-     }).to(".spnBB",{
-    rotationZ:-16,duration:9
- })  
- })
- document.addEventListener("scroll",()=>{ 
-    var rect = document.querySelector(".boxes").getBoundingClientRect();
-console.log(rect.top, rect.left);
-
+       stagger:0.3, scale:2,opacity: 0,duration:16, ease:"elastic.out(1,0.3)",boxShadow:"inset 0px 0px 190px -10px white, 20px 20px 0px rgba(0, 0, 0, 0.357)"
+     })
  }) 
+    var chargingTL=new TimelineLite(
+        {
+            scrollTrigger: {
+                trigger: ".charging",
+                start: "+="+1300,
+                end: "+="+400,
+                scrub: true,
+                stagger:0.4,
+               
+                
+            }
+        })
+        chargingTL.from(".charging",{
+           width:"0px"
+         })
+    
  var spnCtl = new TimelineLite({
     scrollTrigger: {
         trigger: ".spnC",
-        start: "+="+2000,
-        end: "+="+400,
+        start: "+="+2120,
+        end: "+="+930,
         scrub: true,
         markers: false,
 
@@ -161,6 +185,55 @@ console.log(rect.top, rect.left);
 );
 
 spnCtl.from(".spnC", {
-    scale: 0, opacity: 0.3, rotationZ: 30, ease: "bounce.out", duration: 3
-})
- 
+    scale: 0, opacity: 0.3, rotationZ: 30, ease: "bounce.out", duration: 3,boxShadow: "inset 0px 0px 190px -120px white, 0px 0px 0px rgba(0, 0, 0, 0.357)"
+}).fromTo(".spnD",{boxShadow: "inset 0px 0px 190px -120px white, 0px 0px 0px rgba(0, 0, 0, 0.357)"},{
+    x:"105%", duration:6, ease: "slow(0.1,1,false)", delay:2,boxShadow:  "inset 0px 0px 190px -20px rgba(255, 255, 255, 0.272), 5px 5px 0px rgba(0, 0, 0, 0.357)"
+}).fromTo(".spnE",{boxShadow: "inset 0px 0px 190px -120px white, 0px 0px 0px rgba(0, 0, 0, 0.357)"},{
+    y:"105%", duration:6, ease: "elastic.out",immediateRender:true ,
+    boxShadow:  "inset 0px 0px 190px -20px rgba(255, 255, 255, 0.272), 5px 5px 0px rgba(0, 0, 0, 0.357)"
+}).to(".spnE",{
+    x:"105%", duration:5, ease: "elastic.out"
+}) 
+
+const splitCharsColor = document.querySelectorAll(".choices");
+
+splitCharsColor.forEach((char) => {
+    const colorClass = char.classList[1];
+    const text = new SplitType(char, { types: 'words,chars' });
+
+    text.chars.forEach((char, index) => { 
+        
+        gsap.fromTo(
+            char, 
+            {
+                color: "black",
+                duration: 2,
+                delay: index * 0.1,
+                y: 0,
+                rotationZ: 0
+            },
+            {
+                color: colorClass,
+                duration: 2,
+                delay: index * 0.1,
+                y: 0,
+                rotationZ: 0,
+                repeat: -1,
+                yoyo: true
+            }
+        );
+        gsap.to(char, {
+            duration: 1,
+            x: -1100, // ajuste a quantidade de deslocamento conforme necessário
+            delay: index * 0.1, // ajuste o atraso conforme necessário
+            yoyo: true,
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: char,
+                start: "top 95%", // ajuste a posição de início conforme necessário
+                end: "top 100%", // ajuste a posição de fim conforme necessário
+                scrub:false
+            }
+        });
+    });
+});
